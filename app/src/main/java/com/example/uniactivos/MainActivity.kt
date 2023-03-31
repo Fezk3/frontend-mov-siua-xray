e52b8e16cd5c49918e069a09236f01519c5a7eca
Victor Segura <seguravictor57@gmail.com>
2023-03-30 20:29:56 -0600

    mainactivity con contenedor para fragments

--- app/src/main/java/com/example/uniactivos/MainActivity.kt
+++ app/src/main/java/com/example/uniactivos/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.uniactivos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // cargando el fragmento de login
+        supportFragmentManager.commit {
+            replace<Login>(R.id.frameContainer)
+            setReorderingAllowed(true)
+            addToBackStack("replacement")
+        }
+
     }
 }
