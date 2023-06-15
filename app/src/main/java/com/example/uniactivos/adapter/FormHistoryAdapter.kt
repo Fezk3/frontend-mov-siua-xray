3b3b2da752f54e38ceeb4ece72a49f3f8d1e17ac
Victor Segura <seguravictor57@gmail.com>
2023-06-15 11:20:02 -0600

    formhistory

--- app/src/main/java/com/example/uniactivos/adapter/FormHistoryAdapter.kt
+++ app/src/main/java/com/example/uniactivos/adapter/FormHistoryAdapter.kt
@@ -1,4 +1,40 @@
 package com.example.uniactivos.adapter
 
-class FormHistoryAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.uniactivos.databinding.FormhisItemBinding
+import com.example.uniactivos.model.FormHistoryDetails
+
+class FormHistoryAdapter : RecyclerView.Adapter<MainViewHolder2>() {
+
+    private var form = mutableListOf<FormHistoryDetails>()
+
+    fun setFormList(form: List<FormHistoryDetails>) {
+        this.form = form.toMutableList()
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder2 {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = FormhisItemBinding.inflate(inflater, parent, false)
+
+        return MainViewHolder2(binding)
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder2, position: Int) {
+        val formr = form[position]
+        holder.binding.id.text = formr.id.toString()
+        holder.binding.formid.text = formr.form?.id.toString()
+        holder.binding.user.text = formr.user?.firstName.toString()
+        holder.binding.date.text = formr.date.toString()
+        holder.binding.state.text = formr.state?.description.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return form.size
+    }
 }
+class MainViewHolder2(
+    val binding: FormhisItemBinding
+) : RecyclerView.ViewHolder(binding.root)
--- app/src/main/java/com/example/uniactivos/view/fragments/GuardReports.kt
+++ app/src/main/java/com/example/uniactivos/view/fragments/GuardReports.kt
@@ -5,17 +5,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.uniactivos.adapter.FormHistoryAdapter
 import com.example.uniactivos.databinding.FragmentGuardReportsBinding
+import com.example.uniactivos.model.providers.FormHistoryProvider
+import com.example.uniactivos.viewmodel.FormHistoryViewModel
 
 class GuardReports : Fragment(){
     private var _binding: FragmentGuardReportsBinding? = null
     private val binding get() = _binding!!
+
+    val formhisViewModel = FormHistoryViewModel()
+    val adapter = FormHistoryAdapter()
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentGuardReportsBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        binding.rvrepos.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvrepos.adapter = adapter
+
+        formhisViewModel.formHistoryList.observe(viewLifecycleOwner) {
+            adapter.setFormList(it)
+        }
+
+        formhisViewModel.findAllFormHis()
+
         return view
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
