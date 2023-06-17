fdd802ed15ebcbbb2eb1ae96374ea7454c5978f0
Mauren M <mau18.m@gmail.com>
2023-06-17 00:03:03 -0600

    finishing staticTeacher recycler view

--- app/src/main/java/com/example/uniactivos/view/fragments/TeacherStaticForm.kt
+++ app/src/main/java/com/example/uniactivos/view/fragments/TeacherStaticForm.kt
@@ -5,19 +5,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.uniactivos.R
+import com.example.uniactivos.adapter.StaticAssetAdapter
 import com.example.uniactivos.databinding.FragmentTeacherStaticFormBinding
+import com.example.uniactivos.viewmodel.StaticAssetViewModel
 
 class TeacherStaticForm : Fragment(){
     private var _binding: FragmentTeacherStaticFormBinding? = null
     private val binding get() = _binding!!
+
+    private val statAssetViewModel: StaticAssetViewModel by viewModels()
+    private val adapter: StaticAssetAdapter = StaticAssetAdapter()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentTeacherStaticFormBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        binding.rvStaticAsset.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvStaticAsset.adapter = adapter
+
+        statAssetViewModel.assetDetailList.observe(viewLifecycleOwner) {
+            adapter.setAssetList(it)
+        }
+
+        statAssetViewModel.findAllStatAssets()
+
         return view
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
