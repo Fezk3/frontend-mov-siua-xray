3350903c5b3cfe4950001dd86ac88c2d23ffdee9
Victor Segura <seguravictor57@gmail.com>
2023-06-21 14:44:32 -0600

    logout

--- app/src/main/java/com/example/uniactivos/view/fragments/Profile.kt
+++ app/src/main/java/com/example/uniactivos/view/fragments/Profile.kt
@@ -6,9 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.uniactivos.databinding.FragmentProfileBinding
+import com.example.uniactivos.viewmodel.LoginViewModel
 
 class Profile : Fragment(){
     private var _binding: FragmentProfileBinding? = null
+    private lateinit var loginViewModel: LoginViewModel
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,7 +18,15 @@ class Profile : Fragment(){
     ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val view = binding.root
+
+
+        binding.btnChangePass.setOnClickListener {
+            loginViewModel.logout()
+            activity?.finish()
+        }
+
         return view
+
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
