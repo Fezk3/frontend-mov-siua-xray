388eab5685d9c75018ad6a957e3a3e40beb2185d
Victor Segura <seguravictor57@gmail.com>
2023-06-23 18:56:30 -0600

    cargando pendings

--- app/src/main/java/com/example/uniactivos/view/fragments/PendingAssets.kt
+++ app/src/main/java/com/example/uniactivos/view/fragments/PendingAssets.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.uniactivos.R
@@ -14,7 +15,10 @@ import com.example.uniactivos.adapter.FormHistoryPendingAdapter
 import com.example.uniactivos.adapter.ScheduleAdapter
 import com.example.uniactivos.databinding.FragmentMyScheduleBinding
 import com.example.uniactivos.databinding.FragmentPendingAssetsBinding
+import com.example.uniactivos.repository.FormHistoryRepository
+import com.example.uniactivos.service.FormHistoryService
 import com.example.uniactivos.viewmodel.ClassroomViewModel
+import com.example.uniactivos.viewmodel.FormHistoryFactory
 import com.example.uniactivos.viewmodel.FormHistoryViewModel
 import com.example.uniactivos.viewmodel.ScheduleViewModel
 
@@ -22,7 +26,7 @@ class PendingAssets : Fragment(){
     private var _binding: FragmentPendingAssetsBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var formHistoryVM: FormHistoryViewModel
+    private lateinit var formHistoryViewModel: FormHistoryViewModel
     
     private val adapter: FormHistoryPendingAdapter = FormHistoryPendingAdapter()
 
@@ -36,11 +40,16 @@ class PendingAssets : Fragment(){
         binding.rvFormHistoryGuard.layoutManager = LinearLayoutManager(requireContext())
         binding.rvFormHistoryGuard.adapter = adapter
 
-        formHistoryVM.formHistoryList.observe(viewLifecycleOwner) {
+        val formHistoryService = FormHistoryService.getInstance()
+        val formhistoryrepo = FormHistoryRepository(formHistoryService)
+
+        formHistoryViewModel = ViewModelProvider(this, FormHistoryFactory(formhistoryrepo)).get(FormHistoryViewModel::class.java)
+
+        formHistoryViewModel.formHistoryList.observe(viewLifecycleOwner) {
             adapter.setFormList(it)
         }
 
-        formHistoryVM.getAllPendingFormHistory()
+        formHistoryViewModel.getAllPendingFormHistory()
 
         return view
     }
